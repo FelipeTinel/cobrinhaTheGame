6d2de476494d062d56d57295e3091ede23203692
Mirella Costa <mirellabcost@gmail.com>
2025-11-27 21:27:51 -0300

    criei a telinha

--- baseGame.cpp
+++ baseGame.cpp
@@ -1,10 +1,23 @@
 #include <raylib.h>
-#include <stdio.h>
+#include <iostream>
+
+using namespace std;
 
 int main () {
 
-    C
+int cellsize = 32;
+int cellcount = 30;
+cout << "Iniciando o jogo..." << endl;
+InitWindow(cellsize*cellcount, cellsize*cellcount, "Jogo da Cobrinha");
+SetTargetFPS(60);
 
 
+while (!WindowShouldClose()) {
+    BeginDrawing();
+    EndDrawing();
+}
+
+CloseWindow();
+return 0; 
 
 }
